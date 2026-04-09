c5fb9cbd64f888822d6f1282381de6bd95492ace
light610 <57819098+light610@users.noreply.github.com>
2026-04-09 22:40:41 +0800

    Update tusb_config.h

--- src/tusb_config.h
+++ src/tusb_config.h
@@ -5,29 +5,19 @@
 extern "C" {
 #endif
 
-//--------------------------------------------------------------------
-// COMMON CONFIGURATION
-//--------------------------------------------------------------------
 #define CFG_TUSB_RHPORT0_MODE       OPT_MODE_DEVICE
 #define CFG_TUSB_OS                 OPT_OS_PICO
-
-// 启用设备栈
 #define CFG_TUD_ENABLED             1
 
-//--------------------------------------------------------------------
-// DEVICE CONFIGURATION
-//--------------------------------------------------------------------
 #define CFG_TUD_HID                 1
 #define CFG_TUD_CDC                 0
 #define CFG_TUD_MSC                 0
 #define CFG_TUD_MIDI                0
 #define CFG_TUD_VENDOR              0
 
-// HID 缓冲大小
 #define CFG_TUD_HID_EP_BUFSIZE      64
 
 #ifdef __cplusplus
 }
 #endif
-
 #endif
